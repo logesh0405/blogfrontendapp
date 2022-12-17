89f5d235479007d37a05cd33f28a4cf21beaa677
chitta <chittaranjanghosh7@gmail.com>
2022-12-17 11:48:10 +0530

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,52 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AddcommentComponent } from './component/addcomment/addcomment.component';
+import { AddpostComponent } from './component/addpost/addpost.component';
+import { PostlistComponent } from './component/postlist/postlist.component';
+import { SigninComponent } from './component/signin/signin.component';
+import { SignupComponent } from './component/signup/signup.component';
+import { SinglepostComponent } from './component/singlepost/singlepost.component';
+import { AuthGuard } from './security/auth.guard';
+
+const routes: Routes = [
+ {
+   path:'',
+   component:PostlistComponent,
+   
+ },
+ {
+  path:'single-post',
+  component:SinglepostComponent
+},
+{
+  path:'posts',
+  component:PostlistComponent
+},
+
+{
+  path:'signup',
+  component:SignupComponent
+},
+
+{
+  path:'signin',
+  component:SigninComponent
+},
+{
+  path:'add-post',
+  component:AddpostComponent,
+  canActivate: [AuthGuard]
+},
+
+{
+  path:'add-comment',
+  component:AddcommentComponent,
+  canActivate: [AuthGuard]
+}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
